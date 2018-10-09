6e130f7e82fc994a6738fc0fa9e39221d3824a71
hujunhui123 <hujunhui123@outlook.com>
2018-10-09 14:53:11 +0800

    修改文件上传逻辑

--- plane-common/src/main/java/hust/plane/utils/Upload.java
+++ plane-common/src/main/java/hust/plane/utils/Upload.java
@@ -17,8 +17,8 @@ public class Upload {
      * 上传文件
 
      * @param serverPath    服务器地址:(http://172.16.5.102:8090/)
-     * @param path             文件路径（不包含服务器地址：upload/）
-     * @param
+     * @param path            文件路径（不包含服务器地址：ImageTask/）
+     * @param  taskDir        任务文件夹
      * @return
      */
     public static String upload(Client client, MultipartFile file, String serverPath, String path,String taskDir){
@@ -36,12 +36,14 @@ public class Upload {
         String extension = FilenameUtils.getExtension(file.getOriginalFilename());
         // 文件名
         String fileName = formatDate + str + "." + extension;
-        //相对路径
-        String relaPath = path + taskDir+ "/"+"ImageResource" +"/"+ fileName;
+
+        //相对路径   例: ImageTask/23/ImageResource/123.jpg
+
+        String relativePath = path + taskDir+ "/"+"ImageResource" +"/"+ fileName;
 
 //        String a = serverPath + path.substring(0, path.lastIndexOf("/"));
 //         String serverFileAddress = "D:"+File.separator+"Games"+File.separator+taskDir;
-        StringBuilder taskFileAddress = new StringBuilder();
+       /* StringBuilder taskFileAddress = new StringBuilder();
         taskFileAddress.append(File.separator).append("home").append(File.separator).append("gxdx_ai").append(File.separator).append("file-workspace")
                 .append(File.separator).append("ImageTask").append(File.separator).append(taskDir);//任务文件夹地址
         
@@ -88,17 +90,17 @@ public class Upload {
                   System.out.println("文件夹权限授予失败");
               }
               System.out.println(mkdirs);
-        }
+        }*/
 
         // 另一台tomcat的URL（真实路径）
-        String realPath = serverPath + relaPath;
+        String realPath = serverPath + relativePath;
         // 设置请求路径
         WebResource resource = client.resource(realPath);
 
         // 发送开始post get put（基于put提交）
         try {
             resource.put(String.class, file.getBytes());
-            return fileName+";"+relaPath+";"+realPath;
+            return fileName+";"+relativePath+";"+realPath;
         } catch (IOException e) {
             e.printStackTrace();
             return "";
@@ -107,7 +109,7 @@ public class Upload {
 
     /**
      * 删除文件
-     * @param filePath（文件完整地址：http://172.16.5.102:8090/upload/1234.jpg）
+     * @param filePath（文件完整地址：http://172.16.5.102:8090/ImageTask/23/TaskResource/1234.jpg）
      * @return
      */
     public static String delete(String filePath){
