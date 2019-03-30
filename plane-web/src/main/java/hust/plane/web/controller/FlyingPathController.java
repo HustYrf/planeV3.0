a52c78c10e8a831bef7b4d380c792e5d573f4b35
zyuanhui <1359871095@qq.com>
2019-03-30 23:51:28 +0800

    航迹规划的预览功能，在界面上预览飞行过程。

--- plane-web/src/main/java/hust/plane/web/controller/FlyingPathController.java
+++ plane-web/src/main/java/hust/plane/web/controller/FlyingPathController.java
@@ -34,58 +34,62 @@ import java.util.List;
 
 @Controller
 public class FlyingPathController {
-    private static final Logger logger = LoggerFactory.getLogger(FlyingPathController.class);
-    @Autowired
-    private FlyingPathService flyingPathServiceImpl;
+	private static final Logger logger = LoggerFactory.getLogger(FlyingPathController.class);
+	@Autowired
+	private FlyingPathService flyingPathServiceImpl;
 
-    @Autowired
-    public RouteService routeServiceImpl;
+	@Autowired
+	public RouteService routeServiceImpl;
 
-    @Autowired
-    public TaskService taskServiceImpl;
+	@Autowired
+	public TaskService taskServiceImpl;
 
-	/*@Autowired
-	private AirportService airportServiceImpl;*/
+	/*
+	 * @Autowired private AirportService airportServiceImpl;
+	 */
 
-    //跳转飞行路径导入页面
-    @RequestMapping("/flyingPathImport")
-    public String toflyingPathImport(Model model) {
-        model.addAttribute("curNav", "flyingPathImport");
-        return "importFlyingPath";
-    }
+	// 跳转飞行路径导入页面
+	@RequestMapping("/flyingPathImport")
+	public String toflyingPathImport(Model model) {
+		model.addAttribute("curNav", "flyingPathImport");
+		return "importFlyingPath";
+	}
 
-    //提供 飞行路径模板txt下载
-    @RequestMapping("/flyingPathTemplateDownloed")
-    public void flyingPathExcelDownloed(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	// 提供 飞行路径模板txt下载
+	@RequestMapping("/flyingPathTemplateDownloed")
+	public void flyingPathExcelDownloed(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        String basepath = request.getSession().getServletContext().getRealPath("");
-        //String basepath = request.getSession().getServletContext().getRealPath("/WEB-INF/ftl"); 
-        File file = null;
-        InputStream fin = null;
-        ServletOutputStream out = null;
-        try {
-            // 调用工具类的createDoc方法生成excel文档
-            file = new File(basepath + file.separator + "flyingpathTemplate.txt");
-            fin = new FileInputStream(file);
+		String basepath = request.getSession().getServletContext().getRealPath("");
+		// String basepath = request.getSession().getServletContext().getRealPath("/WEB-INF/ftl"); 
+		File file = null;
+		InputStream fin = null;
+		ServletOutputStream out = null;
+		try {
+			// 调用工具类的createDoc方法生成excel文档
+			file = new File(basepath + file.separator + "flyingpathTemplate.txt");
+			fin = new FileInputStream(file);
 
-            response.setCharacterEncoding("utf-8");
-            response.setContentType("text/plain;charset=utf-8");
-            // 设置浏览器以下载的方式处理该文件名
-            response.setHeader("Content-Disposition", "attachment;filename=".concat(String.valueOf(URLEncoder.encode("flyingpathTemplate.txt", "UTF-8"))));
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("text/plain;charset=utf-8");
+			// 设置浏览器以下载的方式处理该文件名
+			response.setHeader("Content-Disposition", "attachment;filename="
+					.concat(String.valueOf(URLEncoder.encode("flyingpathTemplate.txt", "UTF-8"))));
 
-            out = response.getOutputStream();
-            byte[] buffer = new byte[1024];  // 缓冲区
-            int bytesToRead = -1;
-            // 通过循环将读入的excel文件的内容输出到浏览器中
-            while ((bytesToRead = fin.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesToRead);
-            }
-        } finally {
-            if (fin != null) fin.close();
-            if (out != null) out.close();
-        }
+			out = response.getOutputStream();
+			byte[] buffer = new byte[1024]; // 缓冲区
+			int bytesToRead = -1;
+			// 通过循环将读入的excel文件的内容输出到浏览器中
+			while ((bytesToRead = fin.read(buffer)) != -1) {
+				out.write(buffer, 0, bytesToRead);
+			}
+		} finally {
+			if (fin != null)
+				fin.close();
+			if (out != null)
+				out.close();
+		}
 
-    }
+	}
 //    //导出飞行路径kml
 //    @RequestMapping("/importPlanePath")
 //    @ResponseBody
@@ -98,80 +102,89 @@ public class FlyingPathController {
 //        return new JsonView(0).toString();
 //    }
 
-    //返回设定飞行路径页面，返回所有路由数据，并在前台显示
-    @RequestMapping("/setFlyingPath")
-    public String doSetFlyPath(Model model) {
-        List<Route> allRoute = routeServiceImpl.getAllRoute();
-        List<RouteVO> routeList = new ArrayList<RouteVO>();
-        for (int i = 0; i < allRoute.size(); i++) {
+	// 返回设定飞行路径页面，返回所有路由数据，并在前台显示
+	@RequestMapping("/setFlyingPath")
+	public String doSetFlyPath(Model model) {
+		List<Route> allRoute = routeServiceImpl.getAllRoute();
+		List<RouteVO> routeList = new ArrayList<RouteVO>();
+		for (int i = 0; i < allRoute.size(); i++) {
 
-            RouteVO routeVo = new RouteVO(allRoute.get(i));
-            routeList.add(routeVo);
-        }
-        model.addAttribute("routeList", JsonUtils.objectToJson(routeList));
-        model.addAttribute("curNav", "setFlyPath");
-        return "setFlyingPath";
-    }
+			RouteVO routeVo = new RouteVO(allRoute.get(i));
+			routeList.add(routeVo);
+		}
+		model.addAttribute("routeList", JsonUtils.objectToJson(routeList));
+		model.addAttribute("curNav", "setFlyPath");
+		return "setFlyingPath";
+	}
 
-    //获取前台传输的路径字符串
-    @RequestMapping("/doSetFlyPath")
-    @ResponseBody
-    public String doSetFlyPath(FlyingPath flyingPath) {
+	// 获取前台传输的路径字符串
+	@RequestMapping("/doSetFlyPath")
+	@ResponseBody
+	public String doSetFlyPath(FlyingPath flyingPath) {
 
-        if (flyingPathServiceImpl.insertFlyingPath(flyingPath) == true)
-            return "success";
-        else
-            return "failed";
-    }
+		if (flyingPathServiceImpl.insertFlyingPath(flyingPath) == true)
+			return "success";
+		else
+			return "failed";
+	}
 
-    //查询所有的飞行路径列表  分页查询
-    @RequestMapping("/doGetFlyingPathList")
-    public String doGetFlyPathListQueryPage(FlyingPath flyingPath, TailPage<FlyingPath> page, Model model) {
+	// 查询所有的飞行路径列表 分页查询
+	@RequestMapping("/doGetFlyingPathList")
+	public String doGetFlyPathListQueryPage(FlyingPath flyingPath, TailPage<FlyingPath> page, Model model) {
 
-        if (flyingPath.getName() == null || flyingPath.getName() == "") {
-            flyingPath.setName(null);
-        }
-        model.addAttribute("inputName", flyingPath.getName());
+		if (flyingPath.getName() == null || flyingPath.getName() == "") {
+			flyingPath.setName(null);
+		}
+		model.addAttribute("inputName", flyingPath.getName());
 
-        page = flyingPathServiceImpl.queryFlyingPathWithPage(flyingPath, page);
+		page = flyingPathServiceImpl.queryFlyingPathWithPage(flyingPath, page);
 
-        model.addAttribute("page", page);
-        model.addAttribute("curNav", "flyingPathList");
-        return "flyingPathList";
+		model.addAttribute("page", page);
+		model.addAttribute("curNav", "flyingPathList");
+		return "flyingPathList";
 
+	}
 
-    }
+	// 返回一条飞行路径，包括所有信息，在这里使用包装类，用于画图
+	@RequestMapping(value = "/showPlanePath", method = RequestMethod.GET)
+	public String showPlanePath(@RequestParam("id") int id, Model model) {
 
-    //返回一条飞行路径，包括所有信息，在这里使用包装类，用于画图
-    @RequestMapping(value = "/showPlanePath", method = RequestMethod.GET)
-    public String showPlanePath(@RequestParam("id") int id, Model model) {
+		FlyingPath flyingPath = flyingPathServiceImpl.selectByFlyingPathId(id);
+		FlyingPathVO flyingPathVO = new FlyingPathVO(flyingPath);
 
-        FlyingPath flyingPath = flyingPathServiceImpl.selectByFlyingPathId(id);
-        FlyingPathVO flyingPathVO = new FlyingPathVO(flyingPath);
+		model.addAttribute("PlanePath", JsonUtils.objectToJson(flyingPathVO));
+		return "showFlyingPath";
+	}
 
-        model.addAttribute("PlanePath", JsonUtils.objectToJson(flyingPathVO));
-        return "showFlyingPath";
-    }
+	// 路径航迹检查
+	@RequestMapping(value = "/checkPlanePath", method = RequestMethod.GET)
+	public String checkPlanePath(@RequestParam("id") int id, Model model) {
 
-    //删除飞行路径
-    @RequestMapping(value = "/deletePlanePath", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
-    @ResponseBody
-    public String deletePlanePath(FlyingPath flyingPath) {
-        if (flyingPath.getId() != null) {
-            //查询所有的任务中是否含有该飞行路径
-            List<Task> list = taskServiceImpl.getFlyingPathByFlyingId(flyingPath.getId());
-            if (list != null) {
-                return new JsonView(0, "SUCCESS", "任务中有该条飞行路径,删除失败").toString();
-            } else {
-                if (flyingPathServiceImpl.deleteFlyingPath(flyingPath)) {
-                    return new JsonView(0, "SUCCESS", "删除成功").toString();
-                }
-                return new JsonView(0, "SUCCESS", "删除失败").toString();
-            }
-        }
-        return new JsonView(0, "SUCCESS", "未传入飞行路径编号,删除失败").toString();
+		FlyingPath flyingPath = flyingPathServiceImpl.selectByFlyingPathId(id);
+		FlyingPathVO flyingPathVO = new FlyingPathVO(flyingPath);
 
-    }
+		model.addAttribute("PlanePath", JsonUtils.objectToJson(flyingPathVO));
+		return "checkFlyingPath";
+	}
 
+	// 删除飞行路径
+	@RequestMapping(value = "/deletePlanePath", produces = "application/json;charset=UTF-8", method = RequestMethod.POST)
+	@ResponseBody
+	public String deletePlanePath(FlyingPath flyingPath) {
+		if (flyingPath.getId() != null) {
+			// 查询所有的任务中是否含有该飞行路径
+			List<Task> list = taskServiceImpl.getFlyingPathByFlyingId(flyingPath.getId());
+			if (list != null) {
+				return new JsonView(0, "SUCCESS", "任务中有该条飞行路径,删除失败").toString();
+			} else {
+				if (flyingPathServiceImpl.deleteFlyingPath(flyingPath)) {
+					return new JsonView(0, "SUCCESS", "删除成功").toString();
+				}
+				return new JsonView(0, "SUCCESS", "删除失败").toString();
+			}
+		}
+		return new JsonView(0, "SUCCESS", "未传入飞行路径编号,删除失败").toString();
+
+	}
 
 }
